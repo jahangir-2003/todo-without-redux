f3bdc3acf51f43064d10c5ccd024b787777a2473
Jahangir Alam <jahangiralamnke786@gmail.com>
2024-12-31 16:27:18 +0530

    fixed issue and add more feature

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ title, onClick, type }: { title?: String, onClick?: React.events, type?: String }) => {
+const Button = ({ title, onClick, type, className }: { title?: String, onClick?: React.events, type?: String, className?: string }) => {
   return (
-    <button type={type} className="w-[120px] h-10 bg-blue-950 text-white uppercase font-serif rounded-r-md" onClick={onClick}>{title}</button>
+    <button type={type} className={`px-4 h-10 bg-blue-950 text-white uppercase font-serif ${className}`} onClick={onClick}>{title}</button>
   )
 }
 export default Button;
